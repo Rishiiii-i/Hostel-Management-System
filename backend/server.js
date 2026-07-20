b0c2c2878281fbc90d68e0120d2c62415e6738a0
Rishiiii-i <rishimacha00@gmail.com>
2026-07-20 16:56:07 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  // server startup after db setup and name change to hostel-m-s (buffercommands off)
+  // updated backend
   console.log(`Server is running on port ${PORT}`);
 });
